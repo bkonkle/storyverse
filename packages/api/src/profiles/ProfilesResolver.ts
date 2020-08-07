064b4d8b2eef8dbbe3ae068a1f8ec9d670cf3315
Brandon Konkle <brandon@konkle.us>
2020-08-07 08:09:27 -0600

    Last commit on nest.js for now

--- packages/api/src/profiles/ProfilesResolver.ts
+++ packages/api/src/profiles/ProfilesResolver.ts
@@ -1,5 +1,6 @@
-import {Query, Resolver, Args, ID} from '@nestjs/graphql'
+import {Query, Resolver, Args, ID, Int} from '@nestjs/graphql'
 
+import {ManyResponse} from '../query-service/QueryPagination'
 import Profile from './data/Profile'
 import {ProfilesService} from './ProfilesService'
 
@@ -12,5 +13,13 @@ export class ProfilesResolver {
     @Args('id', {type: () => ID}) id: string
   ): Promise<Profile | undefined> {
     return this.service.getOne({where: {id}})
+  }
+
+  @Query(() => Profile)
+  async profiles(
+    @Args('pageSize', {type: () => Int, nullable: true}) pageSize?: number,
+    @Args('page', {type: () => Int, nullable: true}) page?: number
+  ): Promise<ManyResponse<Profile>> {
+    return this.service.getMany({pageSize, page})
   }
 }
