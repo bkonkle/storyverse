1b22a86fff187c90e890cbfdf394764b48e6552e
Brandon Konkle <brandon@konkle.us>
2021-02-01 20:03:21 -0700

    Clean out unused profile code

--- packages/api/src/profiles/ProfileUtils.ts
+++ packages/api/src/profiles/ProfileUtils.ts
@@ -1,20 +1,9 @@
 import {omit} from 'lodash'
 
-import Profile, {TABLE_NAME} from './Profile.entity'
-import {Subject, SubjectInput} from '../authorization/RoleGrantsService'
+import Profile from './Profile.entity'
 
 export const isOwner = (profile: Profile, username?: string) =>
   username && username === profile.user.username
-
-export const subject = (id: string): Subject => ({
-  table: TABLE_NAME,
-  id,
-})
-
-export const subjectInput = (id: string): SubjectInput => ({
-  subjectTable: TABLE_NAME,
-  subjectId: id,
-})
 
 export const censoredFields = ['email', 'userId', 'user'] as const
 export type CensoredProfile = Omit<Profile, typeof censoredFields[number]>
