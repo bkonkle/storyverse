559ae70fda34a158a00fb27603d19ecdde90d35c
Brandon Konkle <brandon@konkle.us>
2021-04-10 11:42:42 -0600

    Add some more config

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const {appRootPath} = require('@nrwl/workspace/src/utils/app-root')
 const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+const {appRootPath} = require('@nrwl/workspace/src/utils/app-root')
 
 const {
   PURGE_MATCH_FILES,
@@ -10,14 +11,51 @@ const configTailwind = require(`${appRootPath}/tools/config/tailwind`)
 module.exports = configTailwind({
   purge: [`${__dirname}/src/${PURGE_MATCH_FILES}`],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.trueGray,
-      teal: colors.teal,
-      red: colors.red,
-    },
+    colors,
   },
+  plugins: [
+    plugin(function ({addComponents, theme}) {
+      const screens = theme('screens', {})
+      addComponents([
+        {
+          '.container': {width: '100%'},
+        },
+        {
+          [`@media (min-width: ${screens.sm})`]: {
+            '.container': {
+              'max-width': '640px',
+            },
+          },
+        },
+        {
+          [`@media (min-width: ${screens.md})`]: {
+            '.container': {
+              'max-width': '768px',
+            },
+          },
+        },
+        {
+          [`@media (min-width: ${screens.lg})`]: {
+            '.container': {
+              'max-width': '1024px',
+            },
+          },
+        },
+        {
+          [`@media (min-width: ${screens.xl})`]: {
+            '.container': {
+              'max-width': '1280px',
+            },
+          },
+        },
+        {
+          [`@media (min-width: ${screens['2xl']})`]: {
+            '.container': {
+              'max-width': '1280px',
+            },
+          },
+        },
+      ])
+    }),
+  ],
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,19 @@ const {PURGE_MATCH_FILES} = require('./tools/config/tailwind/definitions')
 module.exports = {
   purge: [`${__dirname}/libs/shared/components/src/${PURGE_MATCH_FILES}`],
   darkMode: false, // or 'media' or 'class'
-  variants: {
-    extend: {},
-  },
+  variants: [
+    'responsive',
+    'group-hover',
+    'focus-within',
+    'first',
+    'last',
+    'odd',
+    'even',
+    'hover',
+    'focus',
+    'active',
+    'visited',
+    'disabled',
+  ],
   plugins: [require('@tailwindcss/forms')],
 }
