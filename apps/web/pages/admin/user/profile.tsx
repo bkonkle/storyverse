01484e8e1ed9183363b86875bbfceaed9f400806
Brandon Konkle <brandon@konkle.us>
2021-05-04 15:59:42 -0600

    Remove the ProfileSection component

--- apps/web/pages/admin/user/profile.tsx
+++ apps/web/pages/admin/user/profile.tsx
@@ -1,17 +1,36 @@
+import clsx from 'clsx'
 import Head from 'next/head'
 import {withUrqlClient} from 'next-urql'
 
+import {useGetCurrentUserQuery} from '@storyverse/graphql/Schema'
 import Admin from '@storyverse/components/layouts/Admin'
-import {ProfileSection} from '@storyverse/components/admin/sections/user'
+import {
+  ProfileForm,
+  ProfilePreview,
+} from '@storyverse/components/admin/sections/user'
 import {getConfig} from '@storyverse/graphql'
 
 export function UserProfilePage() {
+  const [{fetching, data}] = useGetCurrentUserQuery()
+
+  const user = data?.getCurrentUser
+  const profile = user?.profile
+
   return (
     <Admin>
       <Head>
         <title>Storyverse - User Profile</title>
       </Head>
-      <ProfileSection />
+      {!fetching && user && profile && (
+        <div className={clsx('flex', 'flex-wrap')}>
+          <div className={clsx('w-full', 'lg:w-8/12', 'px-4')}>
+            <ProfileForm user={user} profile={profile} />
+          </div>
+          <div className={clsx('w-full', 'lg:w-4/12', 'px-4')}>
+            <ProfilePreview profile={profile} />
+          </div>
+        </div>
+      )}
     </Admin>
   )
 }
--- libs/components/src/admin/sections/user/index.ts
+++ libs/components/src/admin/sections/user/index.ts
@@ -1,3 +1,2 @@
 export {default as ProfileForm} from './ProfileForm'
 export {default as ProfilePreview} from './ProfilePreview'
-export {default as ProfileSection} from './ProfileSection'
