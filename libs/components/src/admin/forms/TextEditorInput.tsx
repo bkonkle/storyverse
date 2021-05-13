dbaeb3dbc823fc81752471b02301704587f7b663
Brandon Konkle <brandon@konkle.us>
2021-05-12 22:35:29 -0600

    Progress on React-Quill

--- libs/components/src/admin/forms/TextEditorInput.tsx
+++ libs/components/src/admin/forms/TextEditorInput.tsx
@@ -1,12 +1,12 @@
 import {ulid} from 'ulid'
-import {useMemo, useState} from 'react'
-import ReactQuill, {Value} from 'react-quill'
+import {useMemo, useState, useEffect} from 'react'
+import type Quill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 
 import {Input} from './Forms'
 
 export interface TextEditorInputProps
-  extends Omit<ReactQuill.ReactQuillProps, 'onChange'> {
+  extends Omit<Quill.ReactQuillProps, 'onChange'> {
   readOnly?: boolean
   label: string
   error?: string
@@ -15,8 +15,8 @@ export interface TextEditorInputProps
 }
 
 export interface EditorState {
-  value?: Value
-  selection?: ReactQuill.Range
+  value?: Quill.Value
+  selection?: Quill.Range
 }
 
 export default function TextEditorInput({
@@ -29,7 +29,20 @@ export default function TextEditorInput({
   ...rest
 }: TextEditorInputProps) {
   const id = useMemo(() => ulid(), [])
+  const [loading, setLoading] = useState(true)
   const [state, setState] = useState<EditorState>({value})
+
+  useEffect(() => {
+    setLoading(false)
+  }, [setLoading])
+
+  if (loading) {
+    // Skip the server-side render
+    return null
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const ReactQuill: typeof Quill = require('react-quill')
 
   return (
     <Input id={id} label={label} error={error} hint={hint}>
